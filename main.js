82e48dd20c1517bc9a2b86fa5c338038c5deb15d
Leandro <leandrocarvalho1212@gmail.com>
2023-02-06 18:43:34 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function tocaSom (seletorElemento) {
+
+    const elemento = document.querySelector(seletorElemento);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } 
+    else {
+        console.log ('Elemento ou seletor não encontrado')
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// enquanto
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template_string
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }     
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+ 
